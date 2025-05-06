d19c6b22aecb8e1525a885d5c887d540503f0651
vinod-8848digital <vinod@8848digital.com>
2025-05-06 06:06:03 +0530

    fix: revert unintended changes

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -31,8 +31,9 @@ class MaterialRequest(BuyingController):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
-		from erpnext.stock.doctype.material_request_item.material_request_item import MaterialRequestItem
 		from frappe.types import DF
+
+		from erpnext.stock.doctype.material_request_item.material_request_item import MaterialRequestItem
 
 		amended_from: DF.Link | None
 		company: DF.Link
@@ -41,7 +42,7 @@ class MaterialRequest(BuyingController):
 		job_card: DF.Link | None
 		letter_head: DF.Link | None
 		material_request_type: DF.Literal["Purchase", "Material Transfer", "Material Issue", "Manufacture", "Customer Provided"]
-		naming_series: DF.Literal["MAT-MR-.YYYY.-", "MAT/MR/.YYYY.-"]
+		naming_series: DF.Literal["MAT-MR-.YYYY.-"]
 		per_ordered: DF.Percent
 		per_received: DF.Percent
 		scan_barcode: DF.Data | None
@@ -57,7 +58,6 @@ class MaterialRequest(BuyingController):
 		transaction_date: DF.Date
 		transfer_status: DF.Literal["", "Not Started", "In Transit", "Completed"]
 		work_order: DF.Link | None
-	# ruff: noqa
 	# end: auto-generated types
 
 	def check_if_already_pulled(self):
